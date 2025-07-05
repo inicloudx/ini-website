1375b2ec7634519d69409449c66459e28d64545d
littlecurrency <littlecurrency2023@gmail.com>
2025-07-05 14:31:44 +0530

    test

--- inixr_site/settings.py
+++ inixr_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-b%0oo!i(lms@i!ng#7d3ph5kg^s)h452(!evjf&7)rjjp2b0!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inixr.com', 'www.inixr.com']
+ALLOWED_HOSTS = ['inixr.com', 'www.inixr.com', '127.0.0.1']
 
 
 # Application definition
